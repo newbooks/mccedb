359ddc1544a168767fb095a2aa3a9e38152e433a
newbooks <junjun.mao@gmail.com>
2015-01-23 15:34:23 -0500

    Made header a separate file.

--- help.php
+++ help.php
@@ -9,12 +9,7 @@
 <body>
 
 
-<table style="text-align: left; width: 100%;" cellpadding="0" cellspacing="0" id="header">
-    <tr>
-        <td><?php include("logo.php"); ?></td>
-        <td><?php include("search.php"); ?></td>
-    </tr>
-</table>
+<?php include("header.php") ?>
 
 <h2>How to search</h2>
 
--- index.php
+++ index.php
@@ -8,13 +8,7 @@
 
 <body>
 
-<table style="text-align: left; width: 100%;" cellpadding="0" cellspacing="0" id="header">
-    <tr>
-        <td><?php include("logo.php"); ?></td>
-        <td><?php include("search.php"); ?></td>
-    </tr>
-</table>
-
+<?php include("header.php") ?>
 <table style="text-align: left; width: 100%;" cellpadding="0" cellspacing="0">
     <tr>
         <td style="width:20%" id="leftpanel"><?php include("latestentries.php"); ?></td>
