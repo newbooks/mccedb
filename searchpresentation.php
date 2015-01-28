11749750c9e7721ecfb00cde8e59d7dad6c6da79
newbooks <junjun.mao@gmail.com>
2015-01-28 16:43:51 -0500

    Added searchpresentation.php as a way to present initial search result

--- searchpresentation.php
+++ searchpresentation.php
@@ -1,5 +1,18 @@
 <?php
-echo $searchtxt;
+//Decompose search string, multiple match will use the last one in operators
+$operators= array("=", "<", ">", ">=", "<=");
+$trueoperators = array();
+foreach ($operators as $a) {
+    if (strpos($searchtxt,$a) !== FALSE) {
+        $trueoperators[] = $a;
+    }
+}
 
 
-?>
+if (sizeof($trueoperators) == 0) {
+    $operator="";
+} else {
+    $operator=array_pop ( $trueoperators );
+}
+// echo $operator;
+
