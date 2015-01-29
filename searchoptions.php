5335044955ac930e22a126ea5462f9540a93b302
newbooks <junjun.mao@gmail.com>
2015-01-29 16:25:47 -0500

    updated searchoptions.php to compose a session that remembers accumulated search options

--- searchoptions.php
+++ searchoptions.php
@@ -1,13 +1,12 @@
 <?php
 //Current search options
 if (isset($_SESSION["options"])) {
-    $options=array();
+    $options=$_SESSION["options"];
 } else {
     $options=array();
 }
 
 //Decompose search string, multiple match will use the last one in operators
-$searchtxt = $_GET['searchtxt'];
 if (isset($_GET['searchtxt'])) {
     $operators = array("=", "<", ">", ">=", "<=");
     $trueoperators = array();
@@ -18,15 +17,50 @@ if (isset($_GET['searchtxt'])) {
         }
     }
 
-
     if (sizeof($trueoperators) == 0) {
-        $operator = "";
+        $operator = "=";
+        $key = "ALL";
+        $value = "$searchtxt";
     } else {
         $operator = array_pop($trueoperators);
+        $fields = explode($operator,$searchtxt);
+        $key=trim($fields[0]);
+        $value=trim($fields[1]);
     }
-    echo $operator;
-} else {
-    echo "None";
+    //echo $key.$operator,$value."<br>";
+    $new = TRUE;
+    foreach ($options as $a) {
+        if ($a["key"] == $key and $a["operator"] == $operator and $a["value"] == $value) {
+            $new = FALSE;
+            break;
+        }
+    }
+    if ($new) {
+        $options[] = array("key" => $key, "operator" => $operator, "value" => $value);
+    }
+
 }
 
 $_SESSION["options"] = $options;
+print_r($options);
+
+// Refine by
+echo <<<HTML
+<form action="searchresult.php" method="get">
+    <div style="color: #606060">Refine by:</div>
+        <input type="text" name="searchtxt" size="24">
+        <input type="submit" value="Go">
+</form>
+<hr>
+HTML;
+
+echo <<<HTML
+<form action="searchresult.php" method="get">
+    <div style="color: #606060">Structure Method:</div>
+    <hr>
+
+    <div style="color: #606060">Structure Method:</div>
+    <hr>
+
+</form>
+HTML;
