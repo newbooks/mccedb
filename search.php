c3deab3546f181735c3f883a8e363f3d71fc959b
newbooks <junjun.mao@gmail.com>
2015-02-02 16:36:55 -0500

    working on checkbox

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <form action="searchresult.php" method="get">
     <div class="right" style="text-align:right" title="Search example: PDB=1akk">
         Search:
-        <input type="text" name="searchtxt" size="40">
+        <input type="text" name="newsearchtxt" size="40">
         <input type="submit" value="Go">
         <small><a href="help.php">Help</a></small>
     </div>
--- searchoptions.php
+++ searchoptions.php
@@ -7,6 +7,11 @@ if (isset($_SESSION["options"])) {
 }
 
 //Decompose search string, multiple match will use the last one in operators
+if (isset($_GET['newsearchtxt'])) {
+    unset($options);
+    $options=array();
+    $_GET['searchtxt'] = $_GET['newsearchtxt'];
+}
 if (isset($_GET['searchtxt'])) {
     $operators = array("=", "<", ">", ">=", "<=");
     $trueoperators = array();
@@ -36,13 +41,13 @@ if (isset($_GET['searchtxt'])) {
         }
     }
     if ($new) {
-        $options[] = array("key" => $key, "operator" => $operator, "value" => $value);
+        $options[$key] = array("operator" => $operator, "value" => $value);
     }
 
 }
 
 $_SESSION["options"] = $options;
-//print_r($options);
+print_r($options);
 
 // Refine by
 echo <<<HTML
@@ -55,19 +60,27 @@ echo <<<HTML
 HTML;
 
 // Suggestions
-print_r($_GET);
+//print_r($_GET);
+
+echo '<form id="form" action="searchresult.php" method="get">';
+echo '   <div style="color: #606060">Structure Method:</div>';
+echo '   <input type="checkbox" class="checkbox" value="structmethod1=xray" name="searchtxt" ';
+if (isset($options['structmethod1'])) {
+    echo ' checked="checked" ';
+}
+echo '> X Ray</input>';
+echo '    <input type="checkbox" class="checkbox" value="structmethod2=nmr" name="searchtxt" ';
+if (isset($options['structmethod2'])) {
+    echo ' checked="checked" ';
+}
+echo '> NMR</input> <hr> ';
+
+echo '   <div style="color: #606060">pKa Method:</div>';
+echo '   <hr>';
+echo  '</form>';
+
 
 echo <<<HTML
-<form id="form" action="searchresult.php" method="get">
-    <div style="color: #606060">Structure Method:</div>
-    <input type="checkbox" class='checkbox' value="xray" name="structuremethod"/> X Ray</input>
-    <hr>
-
-    <div style="color: #606060">pKa Method:</div>
-    <hr>
-
-</form>
-
 <script type="text/javascript">
     $(function(){
      $('.checkbox').on('change',function(){
