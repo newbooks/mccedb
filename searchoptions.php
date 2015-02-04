e08c93c48775c5bc8562d65ee6838f0646c06769
newbooks <junjun.mao@gmail.com>
2015-02-04 10:14:17 -0500

    working version of remembering checkbox multiselection

--- searchoptions.php
+++ searchoptions.php
@@ -1,6 +1,78 @@
 <?php
 /* Current search options
  */
+
+function Add_option($option, $options) {
+    if (isset($option)) {
+        $operators = array("=", "<", ">", ">=", "<=");
+        $trueoperators = array();
+        $searchtxt = $option;
+        foreach ($operators as $a) {
+            if (strpos($searchtxt, $a) !== FALSE) {
+                $trueoperators[] = $a;
+            }
+        }
+
+        if (sizeof($trueoperators) == 0) {
+            $operator = "=";
+            $key = "ALL";
+            $value = $searchtxt;
+        } else {
+            $operator = array_pop($trueoperators);
+            $fields = explode($operator,$searchtxt);
+            $key=trim($fields[0]);
+            $value=trim($fields[1]);
+        }
+        // Remove redundant options
+        $new = TRUE;
+        foreach ($options as $a) {
+            if (key($a) == $key and $a["operator"] == $operator and $a["value"] == $value) {
+                $new = FALSE;
+                break;
+            }
+        }
+        if ($new) {
+            $options[$key] = array("operator" => $operator, "value" => $value);
+        }
+
+    }
+    return $options;
+}
+
+function Remove_option($option, $options) {
+    if (isset($option)) {
+        $operators = array("=", "<", ">", ">=", "<=");
+        $trueoperators = array();
+        $searchtxt = $option;
+        foreach ($operators as $a) {
+            if (strpos($searchtxt, $a) !== FALSE) {
+                $trueoperators[] = $a;
+            }
+        }
+
+        if (sizeof($trueoperators) == 0) {
+            $operator = "=";
+            $key = "ALL";
+            $value = $searchtxt;
+        } else {
+            $operator = array_pop($trueoperators);
+            $fields = explode($operator,$searchtxt);
+            $key=trim($fields[0]);
+            $value=trim($fields[1]);
+        }
+        while ($a=current($options)) {
+            if (key($options) == $key and $a["operator"] == $operator and $a["value"] == $value) {
+                unset($options[$key]);
+                echo "DONE";
+            }
+            next($options);
+        }
+
+    }
+    return $options;
+}
+
+
 if (isset($_SESSION["options"])) {
     $options=$_SESSION["options"];
 } else {
@@ -11,47 +83,24 @@ if (isset($_SESSION["options"])) {
 if (isset($_GET['newsearchtxt'])) {
     unset($options);
     $options=array();
-    $_GET['refinesearchtxt'] = $_GET['newsearchtxt'];
+    $option = $_GET['newsearchtxt'];
+} elseif (isset($_GET['refinesearchtxt'])) {
+    $option = $_GET['refinesearchtxt'];
 }
-if (isset($_GET['refinesearchtxt'])) {
-    $operators = array("=", "<", ">", ">=", "<=");
-    $trueoperators = array();
-    $searchtxt = $_GET['refinesearchtxt'];
-    foreach ($operators as $a) {
-        if (strpos($searchtxt, $a) !== FALSE) {
-            $trueoperators[] = $a;
-        }
-    }
+$options=Add_option($option, $options);
 
-    if (sizeof($trueoperators) == 0) {
-        $operator = "=";
-        $key = "ALL";
-        $value = $searchtxt;
+//scan checkbox
+$all_items = array("structmethod1=xray", "structmethod2=nmr");
+foreach ($all_items as $item) {
+    if (in_array($item, $_GET['checkboxes'])) {
+        //selected items
+        $options = Add_option($item, $options);
     } else {
-        $operator = array_pop($trueoperators);
-        $fields = explode($operator,$searchtxt);
-        $key=trim($fields[0]);
-        $value=trim($fields[1]);
+        $options = Remove_option($item, $options);
     }
-    // Remove redundant options
-    $new = TRUE;
-    foreach ($options as $a) {
-        if ($a["key"] == $key and $a["operator"] == $operator and $a["value"] == $value) {
-            $new = FALSE;
-            break;
-        }
-    }
-    if ($new) {
-        $options[$key] = array("operator" => $operator, "value" => $value);
-    }
-
 }
 
-// Scan checkbox states
-if (isset($_GET['checkboxsearchtxt'])) {
 
-
-}
 
 // Scan radio selection states
 
@@ -80,12 +129,12 @@ HTML;
 
 echo '<form id="form" action="searchresult.php" method="get">';
 echo '   <div style="color: #606060">Structure Method:</div>';
-echo '   <input type="checkbox" class="checkbox" value="structmethod1=xray" name="searchtxt" ';
+echo '   <input type="checkbox" class="checkbox" value="structmethod1=xray" name="checkboxes[]" ';
 if (isset($options['structmethod1'])) {
     echo ' checked="checked" ';
 }
 echo '> X Ray</input>';
-echo '    <input type="checkbox" class="checkbox" value="structmethod2=nmr" name="searchtxt" ';
+echo '    <input type="checkbox" class="checkbox" value="structmethod2=nmr" name="checkboxes[]" ';
 if (isset($options['structmethod2'])) {
     echo ' checked="checked" ';
 }
