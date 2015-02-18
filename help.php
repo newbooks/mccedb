0adff336e9db92a11b4313a6f82b17d873989edb
newbooks <junjun.mao@gmail.com>
2015-02-18 14:53:30 -0500

    Added search level column

--- help.php
+++ help.php
@@ -53,13 +53,15 @@
 <h3>Supported keywords are (not case sensitive):</h3>
 <table border="1" cellpadding="2" cellspacing="0" width="100%">
     <tr>
-        <th>Keyword</th>
-        <th>Supported operators</th>
-        <th>Value</th>
+        <th style="width:20%">Keyword</th>
+        <th style="width:10%">Search level*</th>
+        <th style="width:10%">Supported operators</th>
+        <th style="width:20%">Value</th>
         <th>Explanation</th>
     </tr>
     <tr>
         <td></td>
+        <td>Protein</td>
         <td></td>
         <td>string</td>
         <td>This will be used to search PDB ID, protein name, taxonomy and remark fields.</td>
@@ -67,114 +69,132 @@
 
     <tr>
         <td>PDB</td>
+        <td>Protein</td>
         <td>=</td>
         <td>string</td>
         <td>An optional chain ID can be appended with separator.</td>
     </tr>
     <tr>
         <td>PROTEIN</td>
+        <td>Protein</td>
         <td>=</td>
         <td>string</td>
         <td>String in protein name.</td>
     </tr>
     <tr>
         <td>TAXONOMY</td>
+        <td>Protein</td>
         <td>=</td>
         <td>string</td>
         <td>String in source organism's scientific name.</td>
     </tr>
     <tr>
         <td>STRUCTURE METHOD</td>
+        <td>Protein</td>
         <td>=</td>
         <td>"X RAY" or "NMR"</td>
         <td>Structure method</td>
     </tr>
     <tr>
         <td>PKA METHOD</td>
+        <td>Protein</td>
         <td>=</td>
         <td>"MCCE" or "Experiment"</td>
         <td>pKa method</td>
     </tr>
     <tr>
         <td>EPSILON</td>
+        <td>Protein</td>
         <td>=</td>
         <td>"4.0" or "8.0"</td>
         <td>Solution dielectric constant pKa is calculated at.</td>
     </tr>
     <tr>
         <td>CHAIN IDS</td>
+        <td>Residue</td>
         <td>=</td>
         <td>characters separate by ","</td>
         <td>Chain IDs. </td>
     </tr>
     <tr>
         <td>STRUCTURE SIZE</td>
+        <td>Protein</td>
         <td><,>,=,<=,>=</td>
         <td>integer</td>
         <td>Structure size in number of residues.</td>
     </tr>
     <tr>
         <td>RESOLUTION</td>
+        <td>Protein</td>
         <td><,></td>
         <td>floating point number</td>
         <td>Resolution in Angstroms by X-ray crystallography. </td>
     </tr>
     <tr>
         <td>MODEL</td>
+        <td>Protein</td>
         <td><,>,=,<=,>=</td>
         <td>integer</td>
         <td>Model number of NMR structures. </td>
     </tr>
     <tr>
         <td>ISOELECTRIC POINT</td>
+        <td>Protein</td>
         <td><,></td>
         <td>floating point number</td>
         <td>Calculated isoelectric point of the protein. </td>
     </tr>
     <tr>
         <td>REMARK</td>
+        <td>Protein</td>
         <td>=</td>
         <td>string</td>
         <td>String appear in remarks.</td>
     </tr>
     <tr>
         <td>RESIDUE</td>
+        <td>Residue</td>
         <td>=</td>
         <td>string</td>
         <td>3-character residue name.</td>
     </tr>
     <tr>
         <td>SEQUENCE</td>
+        <td>Residue</td>
         <td><. =, ></td>
         <td>integer</td>
         <td>sequence number.</td>
     </tr>
     <tr>
         <td>PKA</td>
+        <td>Residue</td>
         <td><, ></td>
         <td>floating point number</td>
         <td>Residue pKa value</td>
     </tr>
     <tr>
         <td>DSOLV</td>
+        <td>MFE</td>
         <td><,></td>
         <td>floating point number</td>
         <td>Residue desolvation energy in pH unit, a measurement of how deep a residue is buried.</td>
     </tr>
     <tr>
         <td>TOTALPW</td>
+        <td>MFE</td>
         <td><,></td>
         <td>floating point number</td>
         <td>Total pairwise residue interaction in pH unit.</td>
     </tr>
     <tr>
         <td>PAIRWISE</td>
+        <td>Pairwise</td>
         <td><,></td>
         <td>floating point number</td>
         <td>Pairwise residue interaction in pH unit.</td>
     </tr>
 </table>
-
+<p style="font-size: smaller">* Search levels in order of Protein, Residue, MFE, and Pairwise take more computing resources.</p>
 
 
 <p class="address" style="text-align:right">
