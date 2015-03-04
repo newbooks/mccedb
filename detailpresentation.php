bbf0732a56320efd25fe843929dbec363304829a
newbooks <junjun.mao@gmail.com>
2015-03-04 16:09:11 -0500

    alignment adjustment for  mfe field

--- detailpresentation.php
+++ detailpresentation.php
@@ -1,5 +1,15 @@
+<table style="text-align: left; width: 100%;" cellpadding="0" cellspacing="0" >
+    <tr>
+        <td width="50%">
+            <div id="placeholder" style="width:600px;height:300px">Titration curve</div>
+            <span id="hoverdata">pH=</span><span id="x">0.00</span>, Charge=<span id="y">0.00</span><br>
+        </td>
+        <td style="vertical-align: top; text-align: left">
+            <div id="MFE_DATA">Mean Field Energy Analysis</div>
+            <div id="clickdata">Delta G comes here</div>
+        </td>
+    </tr>
+</table>
 
 
-<div id="placeholder" style="width:600px;height:300px">Titration curve</div>
-<span id="hoverdata">pH=</span><span id="x">0.00</span>, Charge=<span id="y">0.00</span><br>
-<div id="clickdata">Delta G comes here</div>
+
