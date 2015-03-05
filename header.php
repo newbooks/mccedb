b467ba0fdcbf5c6d7bb4981335e9e33acd5d42ae
newbooks <junjun.mao@gmail.com>
2015-03-05 12:52:59 -0500

    mend

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <table style="text-align: left; width: 100%;" cellpadding="0" cellspacing="0" id="header">
     <tr>
         <td><?php include("logo.php"); ?></td>
-        <td><?php include("search.php"); ?></td>
+        <td style="width: 500px"><?php include("search.php"); ?></td>
     </tr>
 </table>
 
