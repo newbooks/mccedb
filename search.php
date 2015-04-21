f192002823038a732f2afb39c74bbcf1b4c394a6
newbooks <junjun.mao@gmail.com>
2015-04-21 16:03:19 -0400

    experimenting autocompletion of the search box

--- search.php
+++ search.php
@@ -1,7 +1,26 @@
+<script>
+    $(function() {
+        var availableTags = [
+            "PDB =",
+            "Protein =",
+            "Taxonomy =",
+            "Structure Method = X RAY",
+            "Structure Method = NMR",
+            "pKa >",
+            "pKa <",
+            "DSOLV >"
+        ];
+        $( "#tags" ).autocomplete({
+            source: availableTags
+        });
+    });
+</script>
+
+
 <form action="searchresult.php" method="post">
     <div class="right">
         Search:
-        <input type="text" placeholder="e.g. PDB=1AKK" name="newsearchtxt" size="40">
+        <input type="text" placeholder="e.g. PDB=1AKK" name="newsearchtxt" size="40" id="tags">
         <input type="submit" value="Go">
         <small><a href="help.php">Help</a></small>
     </div>
--- searchresult.php
+++ searchresult.php
@@ -4,7 +4,10 @@
     <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
     <title>MCCE pKa Database</title>
     <link rel="stylesheet" type="text/css" href="mcce.css"/>
+    <link rel="stylesheet" type="text/css" media="all" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/base/jquery-ui.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
+
 </head>
 
 <body>
